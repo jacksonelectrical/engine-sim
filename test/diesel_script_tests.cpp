30c52a8f664beeeae6e640a3a91e76bb6d3d0b1b
jacksonelectrical <248424962+jacksonelectrical@users.noreply.github.com>
2026-08-03 14:21:18 +1000

    Exercise diesel chamber heat release

--- test/diesel_script_tests.cpp
+++ test/diesel_script_tests.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 
+#include "../include/constants.h"
+#include "../include/units.h"
 #include "../scripting/include/compiler.h"
+
+#include <cmath>
 
 TEST(DieselScriptTests, ReferenceConfigurationCompilesAndBuildsDieselEngine) {
     es_script::Compiler compiler;
@@ -23,6 +27,62 @@ TEST(DieselScriptTests, ReferenceConfigurationCompilesAndBuildsDieselEngine) {
             output.engine->getCombustionEventController()->getType(),
             CombustionEventController::Type::CompressionIgnition);
         EXPECT_TRUE(output.engine->getIntake(0)->m_directInjection);
+
+        Engine *engine = output.engine;
+        Crankshaft *crankshaft = engine->getOutputCrankshaft();
+        CombustionChamber *chamber = engine->getChamber(0);
+        CombustionEventController *controller =
+            engine->getCombustionEventController();
+
+        chamber->m_system.initialize(
+            units::pressure(30.0, units::atm),
+            chamber->getVolume(),
+            units::kelvin(900.0));
+        crankshaft->m_body.v_theta = -units::rpm(1200.0);
+        engine->setThrottle(0.0);
+        controller->m_enabled = true;
+
+        const double fourPi = 4.0 * constants::pi;
+        double injectionAngle = std::fmod(
+            -controller->getTimingAdvance(),
+            fourPi);
+        if (injectionAngle < 0.0) injectionAngle += fourPi;
+
+        const double beforeInjection =
+            injectionAngle - units::angle(1.0, units::deg);
+        crankshaft->m_body.theta +=
+            crankshaft->getCycleAngle() - beforeInjection;
+        controller->reset();
+        crankshaft->m_body.theta -= units::angle(2.0, units::deg);
+        controller->update(0.001);
+
+        const CombustionEventController::Event event =
+            controller->getCombustionEvent(0);
+        EXPECT_TRUE(event.active);
+        EXPECT_EQ(
+            event.kind,
+            CombustionEventController::Event::Kind::DieselInjection);
+        EXPECT_GT(event.fuelMass, 0.0);
+        EXPECT_GT(event.combustionNoise, 0.0);
+
+        if (
+            event.active
+            && event.kind
+                == CombustionEventController::Event::Kind::DieselInjection)
+        {
+            chamber->beginDieselInjection(event);
+            for (int i = 0; i < 200; ++i) {
+                chamber->flow(0.0001);
+            }
+
+            EXPECT_NEAR(
+                chamber->getTotalInjectedFuelMass(),
+                event.fuelMass,
+                event.fuelMass * 1E-6);
+            EXPECT_GT(chamber->m_nBurntFuel, 0.0);
+            EXPECT_GT(chamber->getLastTimestepDieselPressureRise(), 0.0);
+            EXPECT_TRUE(std::isfinite(chamber->m_system.pressure()));
+        }
     }
 
     compiler.destroy();
